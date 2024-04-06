e1dde1df0e090ef545c06576b04f002b80ed1947
Adam Azuddin <05adamaz@gmail.com>
2024-04-07 00:50:13 +0800

    change hamburger menu items to different types of users

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -11,52 +11,71 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useLocation, Link } from 'react-router-dom';
 
-export default function HamburgerMenu() {
+interface LinkItem {
+  text: string;
+  path: string;
+}
+
+const HamburgerMenu: React.FC = () => {
   const [open, setOpen] = React.useState(false);
+  const location = useLocation();
+  const currentPath = location.pathname;
 
   const toggleDrawer = (newOpen: boolean) => () => {
     setOpen(newOpen);
   };
 
+  const links: Record<string, LinkItem[]> = {
+    '/': [
+      { text: 'Home', path: '/' },
+      { text: 'Classes', path: '/classes' },
+      { text: 'Assignments', path: '/assignments' }
+    ],
+    '/teacher': [
+      { text: 'Home', path: '/teacher' },
+      { text: 'Classes', path: '/teacher/classes' },
+      { text: 'Assignments', path: '/teacher/assignments' }
+    ],
+    '/admin': [
+      { text: 'Home', path: '/admin' },
+      { text: 'Manage Users', path: '/admin/manage-users' },
+      { text: 'Attendance', path: '/admin/attendance' },
+      { text: 'Analytics', path: '/admin/analytics' }
+    ]
+  };
+
+  const currentLinks: LinkItem[] = links[currentPath] || [];
+
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
+        {currentLinks.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton component={Link} to={item.path}>
               <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={item.text} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
       <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
     </Box>
   );
 
   return (
     <div>
       <Button onClick={toggleDrawer(true)}>
-        <MenuIcon/>
+        <MenuIcon className=' text-white text-3xl'/>
       </Button>
       <Drawer open={open} onClose={toggleDrawer(false)}>
         {DrawerList}
       </Drawer>
     </div>
   );
-}
+};
+
+export default HamburgerMenu;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,36 +3,47 @@
 import Link from "next/link";
 
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
-import {
-  setLoader,
-} from "@/redux/features/records/recordSlice";
+import { setLoader } from "@/redux/features/records/recordSlice";
 import HamburgerMenu from "./HamburgerMenu";
+import NavProfile from "./NavProfile";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
   const { isSidebarOpen } = useAppSelector((state) => state.record);
   return (
-    <div className="w-full h-[55px] relative bg-[#222]">
-      <div
-        className={`main-container flex flex-row ${
-          !isSidebarOpen ? "justify-start" : "justify-between"
-        } items-center`}
-      >
-        <HamburgerMenu />
-        <Link
-          href="/"
-          onClick={() => dispatch(setLoader(true))}
-          className="w-72 h-[55px]"
-          style={{ fontFamily: "Lobster Two" }}
+    <Router>
+      <div className="w-full h-[55px] relative bg-[#222]">
+        <div
+          className={`main-container flex flex-row ${
+            !isSidebarOpen ? "justify-start" : "justify-between"
+          } items-center`}
         >
-          <h1
-            className={`text-gray-50 text-3xl font-bold pt-2.5`}
+          <HamburgerMenu />
+          <Link
+            href="/"
+            onClick={() => dispatch(setLoader(true))}
+            className="w-72 h-[55px]"
+            style={{ fontFamily: "Lobster Two" }}
           >
-            Afterkelas
-          </h1>
-        </Link>
+            <h1 className={`text-gray-50 text-3xl font-bold pt-2.5`}>
+              Afterkelas
+            </h1>
+          </Link>
+
+          <div
+            className={`ml-20 md:ml-30 flex justify-between items-center w-full ${
+              !isSidebarOpen ? "block" : "hidden"
+            }`}
+          >
+            <div className="relative"></div>
+            <div>
+              <NavProfile />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
