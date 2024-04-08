0ebd4cda18b81128646b811128c7e1ef62de9c25
Adam Azuddin <05adamaz@gmail.com>
2024-04-08 23:08:52 +0800

    cleaned up navprofile\

--- components/NavProfile.tsx
+++ components/NavProfile.tsx
@@ -1,7 +1,5 @@
 "use client";
-import arrowDownSvg from "@/assets/arrowDownWhite.svg";
-import messengerSvg from "@/assets/messenger.svg";
-import notificationSvg from "@/assets/notification.svg";
+
 import userSvg from "@/assets/userIcon.svg";
 import Link from "next/link";
 import Image from "next/image";
@@ -9,33 +7,6 @@ import Image from "next/image";
 const NavProfile = () => {
   return (
     <div className="flex justify-start items-center">
-      <div>
-        <Image
-          priority
-          src={messengerSvg}
-          alt="messenger"
-          className="w-[16px] h-[16px] text-white"
-        />
-      </div>
-      <div className="flex justify-start items-center mx-3">
-        <h3 className="font-normal leading-normal text-[16px] text-white uppercase">
-          en
-        </h3>
-        <Image
-          priority
-          src={arrowDownSvg}
-          alt="arrow-down"
-          className="w-[16px] h-[16px] text-white mx-2"
-        />
-      </div>
-      <div className="text-gray-50">
-        <Image
-          priority
-          src={notificationSvg}
-          alt="notification"
-          className="w-[16px] h-[16px] text-white"
-        />
-      </div>
       <div className="flex justify-start items-center mx-3">
         <Link href="/profile">
             <Image
