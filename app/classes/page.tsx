284e96515480b06b31479a0664a4c9f8bc0e0eef
Adam Azuddin <05adamaz@gmail.com>
2024-04-11 17:22:18 +0800

    added subjects to classes: addmath, accounting, econ

--- app/classes/page.tsx
+++ app/classes/page.tsx
@@ -1,25 +1,16 @@
 import React from "react";
-import Card from "../../components/Card"
+import Card from "../../components/Card";
 
 const page = () => {
   return (
-    <div className="flex mx-6 px-5">
-        <Card
-          subject="Math"
-          color="#FFB6C1 "
-        />
-        <Card
-          subject="Physics"
-          color="#87CEEB"
-        />
-        <Card
-          subject="Chemistry"
-          color="#90EE90"
-        />
-        <Card
-          subject="Biology"
-          color="#FFD700"
-        />
+    <div className="flex mx-6 px-5 flex-wrap">
+      <Card subject="Math" color="#FFB6C1 " />
+      <Card subject="Physics" color="#87CEEB" />
+      <Card subject="Chemistry" color="#90EE90" />
+      <Card subject="Biology" color="#FFD700" />
+      <Card subject="Additional Math" color="#9370DB" />
+      <Card subject="Accounting" color="#4682B4" />
+      <Card subject="Economics" color="#F08080" />
     </div>
   );
 };
