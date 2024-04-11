bf1fe672faa669edb92439661f992cd2cfd32912
Adam Azuddin <05adamaz@gmail.com>
2024-04-11 14:11:25 +0800

    get uid of current teacher

--- app/classes/[subject]/[teacher]/page.tsx
+++ app/classes/[subject]/[teacher]/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client"
+import React from "react";
+import { usePathname } from "next/navigation";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const pathname = usePathname();
+  const pathSegments = pathname.split('/'); // Split the pathname into segments
+  const teacherId = pathSegments[pathSegments.length - 1]; // Access the last segment
 
-export default page
+  console.log(teacherId);
+  return <div>page</div>;
+};
+
+export default page;
