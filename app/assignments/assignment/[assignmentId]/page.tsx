101f6dc24a0510087d6bf649876d6534f7c8761d
Adam Azuddin <05adamaz@gmail.com>
2024-04-14 23:16:01 +0800

    display assignmetntId on page

--- app/assignments/assignment/[assignmentId]/page.tsx
+++ app/assignments/assignment/[assignmentId]/page.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 const page = () => {
+  const pathname = usePathname();
+  const pathSegments = pathname.split("/"); // Split the pathname into segments
+  const assignmentId = pathSegments[pathSegments.length - 1]; // Access the last segment
   return (
-    <div>page</div>
+    <div>{assignmentId}</div>
   )
 }
 
