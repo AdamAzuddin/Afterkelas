72825c529dfb886a40da0199b52546c43882fcca
Adam Azuddin <05adamaz@gmail.com>
2024-04-16 20:39:50 +0800

    added necessary ui for returning studewnt's submission

--- app/assignments/submission/[assignmentId]/[submissionId]/page.tsx
+++ app/assignments/submission/[assignmentId]/[submissionId]/page.tsx
@@ -14,7 +14,7 @@ import {
   DocumentData,
 } from "firebase/firestore"; // Import arrayUnion
 import { db, storage } from "@/app/firebase"; // Import storage from firebase
-import { Paper, Typography } from '@mui/material';
+import { Paper, Typography, Button, TextField } from "@mui/material";
 
 const page = () => {
   const pathname = usePathname();
@@ -27,30 +27,79 @@ const page = () => {
       const submissionQuerySnapshot = await getDocs(
         query(submissionsRef, where("uid", "==", submissionId))
       );
-      const submission = submissionQuerySnapshot.docs[0].data()
-      console.log(submission.assignmentId)
-      setSubmission(submission)
+      const submission = submissionQuerySnapshot.docs[0].data();
+      console.log(submission.assignmentId);
+      setSubmission(submission);
     } catch (error) {
       console.error("Error fetching assignment:", error);
       return null;
     }
   };
 
-  fetchSubmission(submissionId)
+  const handleReturnSubmission = async () => {};
+
+  const handleChangeGrade = async () => {};
+
+  fetchSubmission(submissionId);
   return (
-    <Paper elevation={3} style={{ padding: '20px', marginBottom: '10px' }}>
+    <Paper elevation={3} style={{ padding: "20px", marginBottom: "10px" }}>
       <div>
         <Typography variant="h6">Submission: {submission?.uid}</Typography>
         <Typography>Student ID: {submission?.studentId}</Typography>
         <Typography>Assignment ID: {submission?.assignmentId}</Typography>
-        <Typography>File: {submission?.file}</Typography>
-        <Typography>Has Been Graded: {submission?.hasBeenGraded ? 'Yes' : 'No'}</Typography>
-        {submission?.hasBeenGraded && <Typography>Mark: {submission.mark}</Typography>}
-        {submission?.hasBeenGraded && <Typography>Marked File: {submission.markedFile}</Typography>}
+        <Typography>
+          Has Been Graded: {submission?.hasBeenGraded ? "Yes" : "No"}
+        </Typography>
+        {submission?.hasBeenGraded && (
+          <>
+            <Typography>Mark: {submission.mark}</Typography>
+            <Typography>Marked File: {submission.markedFile}</Typography>
+          </>
+        )}
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            href={submission?.file}
+            target="_blank"
+            style={{ marginBottom: "10px" }}
+          >
+            View Submitted File
+          </Button>
+        </div>
+        <div>
+          <TextField
+            type="number"
+            label="Grade"
+            variant="outlined"
+            style={{ marginBottom: "10px" }}
+            onChange={handleChangeGrade}
+            inputProps={{ min: 0 }}
+          />
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            component="label"
+            style={{ marginBottom: "10px" }}
+          >
+            Upload Marked File
+            <input type="file" hidden />
+          </Button>
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => handleReturnSubmission()}
+            style={{ marginBottom: "10px" }}
+          >
+            Return Submission
+          </Button>
+        </div>
       </div>
     </Paper>
   );
-  
 };
 
 export default page;
