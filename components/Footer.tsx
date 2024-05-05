686da867fd32882c66186035d992af03d4979e51
Adam Azuddin <05adamaz@gmail.com>
2024-05-05 11:51:29 +0800

    edited fotter text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="h-[5rem] flex justify-center items-center text-center mt-5 bg-gray-900">
       <h5 className="text-capitalize text-white m-[0.1rem] leading-[1.25]">
         &copy; {new Date().getFullYear()}{" "}
-        <span className="text-white">Peabux.</span>
+        <span className="text-white">Afterkelas.</span>
       </h5>{" "}
       <h5 className="fs-8 fw-medium text-capitalize text-white">
         All rights reserved
